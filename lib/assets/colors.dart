9a8302b59c768a38b9a83544f756f4bde4ba47bc
AhmetKoca32 <kocaahmetkoca32@gmail.com>
2024-10-18 17:00:39 +0300

    second commit

---
+++ lib/assets/colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color lightGray = Color(0xFFD3D3D3);
+  static const Color mediumGray = Color(0xFFA9A9A9);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color beige = Color(0xFFF5F5DC);
+  static const Color cream = Color(0xFFFDFDD0);
+  static const Color black = Color(0xFF000000);
+}
+
+/*Açık Gri (Arka Plan): #D3D3D3
+Orta Gri (Üst Bar): #A9A9A9
+Beyaz (Başlık Rengi): #FFFFFF
+Bej (Buton 1): #F5F5DC
+Krem (Buton 2): #FFFDD0
+Siyah (Buton Yazı Rengi): #000000*/
---
+++ lib/assets/fonts.dart
@@ -0,0 +1,24 @@
+//pubspec.yaml de tanimlanan font familyleri String degerlere tanimladik.
+//bu sekilde uygulama tasarim ekranında textStyleda kisa ve dinamik tasarimlar
+//yapabiliriz.
+
+class Fonts {
+  static const String poppinsThin = 'Poppins-Thin';
+  static const String poppinsThinItalic = 'Poppins-ThinItalic';
+  static const String poppinsExtraLight = 'Poppins-ExtraLight';
+  static const String poppinsExtraLightItalic = 'Poppins-ExtraLightItalic';
+  static const String poppinsLight = 'Poppins-Light';
+  static const String poppinsLightItalic = 'Poppins-LightItalic';
+  static const String poppinsRegular = 'Poppins-Regular';
+  static const String poppinsItalic = 'Poppins-Italic';
+  static const String poppinsMedium = 'Poppins-Medium';
+  static const String poppinsMediumItalic = 'Poppins-MediumItalic';
+  static const String poppinsSemiBold = 'Poppins-SemiBold';
+  static const String poppinsSemiBoldItalic = 'Poppins-SemiBoldItalic';
+  static const String poppinsBold = 'Poppins-Bold';
+  static const String poppinsBoldItalic = 'Poppins-BoldItalic';
+  static const String poppinsExtraBold = 'Poppins-ExtraBold';
+  static const String poppinsExtraBoldItalic = 'Poppins-ExtraBoldItalic';
+  static const String poppinsBlack = 'Poppins-Black';
+  static const String poppinsBlackItalic = 'Poppins-BlackItalic';
+}
